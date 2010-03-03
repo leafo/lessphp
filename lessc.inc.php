3c9ffc35853f924b6588e89b1c06c1b89d0f6c49
leaf corcoran <leafot@gmail.com>
2010-03-03 08:26:17 -0500

    Finished transition to new parsing model
     * exceptions no longer used to explore parsing expressions
     * about 50% faster
     * some subtle ambiguities cleaned up
     * accessors with will find abstract blocks properly
     * import directive parsing should match all cases now
     * math operators don't need whitespace when touching parens

--- lessc.inc.php
+++ lessc.inc.php
@@ -7,31 +7,20 @@
  * less css compiler
  * adapted from http://lesscss.org/docs.html
  *
- * leaf corcoran <leafo.net>
+ * leaf corcoran <leafot@gmail.com>
+ * leafo.net
  */
 
-
-// future todo: define type names as constants
-
-// todo: potential problem with parse tree search order:
 //
-// #default['color'] is an accessor, but if color is searched
-// first then #def is matched as a color and it returns true and the head is
-// moved to ault['color']; That is then seen as a second value in the list
-//
-// solution, enforce at least a space, } or {, or ; after match
-// i can consume the spaces but not the symbols
-// need more time to think about this, maybe leaving order requirement is good
+// investigate trouble with  
+// fix the alpha value with color when using a percent
 //
 
 class lessc {
 	private $buffer;
 	private $count;
-	private $depth;
 	private $line;
 	private $expandStack;
-
-	private $tests = array();
 
 	private $env = array();
 
@@ -43,10 +32,12 @@ class lessc {
 	);
 	static private $operatorString; // regex string to any of the operators
 
-	static private $dtypes = array('expression', 'variable'); // types with delayed compilation
+	static private $dtypes = array('expression', 'variable'); // types with delayed computation
 	static private $units = array(
 		'px', '%', 'in', 'cm', 'mm', 'em', 'ex', 'pt', 'pc', 's');
 
+	public $importDisabled = false;
+	public $importDir = '';
 
 	// compile chunk off the head of buffer
 	function chunk() {
@@ -54,7 +45,6 @@ class lessc {
 		$s = $this->seek();
 
 		// a property
-		// [keyword] : [propertyValue] ;
 		if ($this->keyword($key) && $this->literal(':') && $this->propertyValue($value) && $this->end()) {
 			$this->append($key, $value);
 
@@ -66,9 +56,22 @@ class lessc {
 			$this->seek($s);
 		}
 
-		// function block
+		// opening abstract block
+		if ($this->tag($tag, true) && $this->argumentDef($args) && $this->literal('{')) {
+			$this->push();
 
-		// regular block
+			// move out of variable scope
+			if ($tag{0} == "@") $tag[0] = "%";
+
+			$this->set('__tags', array($tag));
+			if (isset($args)) $this->set('__args', $args);
+
+			return true;
+		} else {
+			$this->seek($s);
+		}
+
+		// opening css block
 		if ($this->tags($tags) && $this->literal('{')) {
 			//  move @ tags out of variable namespace!
 			foreach($tags as &$tag) {
@@ -83,7 +86,7 @@ class lessc {
 			$this->seek($s);
 		}
 
-		// close block
+		// closing block
 		if ($this->literal('}')) {
 			$tags = $this->multiplyTags();
 			$env = end($this->env);
@@ -115,12 +118,21 @@ class lessc {
 			}
 
 			return isset($out) ? $out : true;
-		} else {
-			$this->seek($s);
-		}
+		} 
 		
 		// import statement
+		if ($this->import($url, $media)) {
+			if ($this->importDisabled) return "/* import is disabled */\n";
 
+			$full = $this->importDir.$url;
+			if (file_exists($file = $full) || file_exists($file = $full.'.less')) {
+				$loaded = $this->removeComments(ltrim(file_get_contents($file).";"));
+				$this->buffer = substr($this->buffer, 0, $this->count).$loaded.substr($this->buffer, $this->count);
+				return true;
+			}
+
+			return '@import url("'.$url.'")'.($media ? ' '.$media : '').";\n";
+		}
 
 		// setting variable
 		if ($this->variable($name) && $this->literal(':') && $this->propertyValue($value) && $this->end()) {
@@ -130,11 +142,83 @@ class lessc {
 			$this->seek($s);
 		}
 
-		// mixin import
+		// mixin/function expand
+		if ($this->tags($tags, true, '>')) {
+			//  move @ tags out of variable namespace
+			foreach($tags as &$tag) {
+				if ($tag{0} == "@") $tag[0] = "%";
+			}
+
+			// look for arguments
+			$this->argumentValues($argv);
+
+			if (!$this->end()) {
+				$this->seek($s);
+				return false; // don't do this here
+			}
+			
+			// find the referenced environment
+			$env = $this->get(array_shift($tags));
+			while ($sub = array_shift($tags)) {
+				if (isset($env[$sub]))  // todo add a type check for environment
+					$env = $env[$sub];
+				else {
+					$env = null;
+					break;
+				}
+			}
+
+			if ($env == null) return true;
+
+			// if we have arguments then insert them
+			if (!empty($env['__args'])) {
+				foreach($env['__args'] as $arg) {
+					$name = $arg[0];
+					$value = is_array($argv) ? array_shift($argv) : null;
+					// copy default value if there isn't one supplied
+					if ($value == null && isset($arg[1]))
+						$value = $arg[1];
+
+					// if ($value == null) continue; // don't define so it can search up
+
+					// create new entry if var doesn't exist in scope
+					if (isset($env['@'.$name])) {
+						array_unshift($env['@'.$name], $value);
+					} else {
+						// new element
+						$env['@'.$name] = array($value);
+					}
+				}
+			}
+
+			// set all properties
+			ob_start();
+			foreach ($env as $name => $value) {
+				// skip the metatdata
+				if (preg_match('/^__/', $name)) continue;
+
+				// if it is a block then render it
+				if (!isset($value[0])) {
+					$rtags = $this->multiplyTags(array($name));
+					echo $this->compileBlock($rtags, $value);
+				}
+
+				// copy the block's data
+				// don't overwrite previous value, look in current env for name
+				if ($this->get($name, array(end($this->env)))) {
+					while ($tval = array_shift($value))
+						$this->append($name, $tval);
+				} else 
+					$this->set($name, $value); 
+			}
+
+			return ob_get_clean();
+		}
 
 		// spare ;
+		if ($this->literal(';')) return true;
 
-		return false;	
+		return false; // couldn't match anything, throw error
 	}
 
 	// recursively find the cartesian product of all tags in stack
@@ -169,28 +253,33 @@ class lessc {
 		return true;
 	}
 
+	// a single expression
 	function expression(&$out) {
 		$s = $this->seek();
+		$needWhite = true;
 		if ($this->literal('(') && $this->expression($exp) && $this->literal(')')) {
 			$lhs = $exp;
+			$needWhite = false;
 		} elseif ($this->seek($s) && $this->value($val)) {
 			$lhs = $val;
 		} else {
 			return false;
 		}
 
-		$out = $this->expHelper($lhs, 0);
+		$out = $this->expHelper($lhs, 0, $needWhite);
 		return true;
 	}
 
 	// resursively parse infix equation with $lhs at precedence $minP
-	function expHelper($lhs, $minP) {
+	function expHelper($lhs, $minP, $needWhite = true) {
 		$ss = $this->seek();
 		// try to find a valid operator
-		while ($this->match(self::$operatorString.'\s+', $m) && self::$precedence[$m[1]] >= $minP) {
+		while ($this->match(self::$operatorString.($needWhite ? '\s+' : ''), $m) && self::$precedence[$m[1]] >= $minP) {
+			$needWhite = true;
 			// get rhs
 			$s = $this->seek();
 			if ($this->literal('(') && $this->expression($exp) && $this->literal(')')) {
+				$needWhite = false;
 				$rhs = $exp;
 			} elseif ($this->seek($s) && $this->value($val)) {
 				$rhs = $val;
@@ -233,16 +322,41 @@ class lessc {
 		return true;
 	}
 
+	// a single value
 	function value(&$value) {
+		/*
+		switch (true) {
+			case $this->unit($value): return true;
+			// ...
+		}
+		 */
+
 		// try a unit
 		if ($this->unit($value)) return true;	
 
 		// accessor 
+		// must be done before color
+		if ($this->accessor($a)) {
+			$tmp = $this->get($a[0]);
+			if (isset($tmp[$a[1]]))
+				$value = end($tmp[$a[1]]); // fails if $tmp isn't an environment
+			return true;
+		}
+		
 		// color
 		if ($this->color($value)) return true;
 
 		// css function
+		if ($this->func($f)) {
+			$value = array('string', $f);
+			return true;
+		}
+
 		// string
+		if ($this->string($tmp, $d)) {
+			$value = array('string', $d.$tmp.$d);
+			return true;
+		}
 
 		// try a keyword
 		if ($this->keyword($word)) {
@@ -259,6 +373,88 @@ class lessc {
 		return false;
 	}
 
+	// an import statement
+	function import(&$url, &$media) {
+		$s = $this->seek();
+		if (!$this->literal('@import')) return false;
+
+		// there are many forms of import
+		// @import "something.css" media;
+		// @import url("something.css") media;
+		// @import url(something.css) media; 
+
+		if ($this->literal('url(')) $parens = true; else $parens = false;
+
+		if (!$this->string($url)) {
+			if ($parens && $this->to(')', $url)) {
+				$parens = false; // got em
+			} else {
+				$this->seek($s);
+				return false;
+			}
+		}
+
+		if ($parens && !$this->literal(')')) {
+			$this->seek($s);
+			return false;
+		}
+
+		// now the rest is media
+		return $this->to(';', $media);
+	}
+
+	// a scoped value accessor
+	function accessor(&$var) {
+		$s = $this->seek();
+		if (!$this->tag($scope, true) || !$this->literal('[')) {
+			$this->seek($s);
+			return false;
+		}
+
+		if ($scope{0} == '@') $scope{0} = '%';
+
+		// either it is a variable or a property
+		// why is a property wrapped in quotes, who knows!
+		if ($this->variable($name)) {
+			$name = '@'.$name;
+		} elseif($this->literal("'") && $this->keyword($name) && $this->literal("'")) {
+			// .. $this->count is messed up if we wanted to test another access type
+		} else {
+			$this->seek($s);
+			return false;
+		}
+
+		if (!$this->literal(']')) {
+			$this->seek($s);
+			return false;
+		}
+
+		$var = array($scope, $name);
+
+		return true;
+	}
+
+	// a string 
+	function string(&$string, &$d = null) {
+		$s = $this->seek();
+		if ($this->literal('"')) {
+			$delim = '"';
+		} else if($this->literal("'")) {
+			$delim = "'";
+		} else {
+			return false;
+		}
+
+		if (!$this->to($delim, $string)) {
+			$this->seek($s);
+			return false;
+		}
+		
+		$d = $delim;
+		return true;
+	}
+
+	// a numerical unit
 	function unit(&$unit, $allowed = null) {
 		if (!$allowed) $allowed = self::$units;
 
@@ -271,6 +467,7 @@ class lessc {
 		return false;
 	}
 
+	// a color
 	function color(&$out) {
 		$color = array('color');
 
@@ -296,6 +493,7 @@ class lessc {
 		} else {
 			// try for rgb(a) function	
 			$s = $this->seek();
+
 			if (!$this->literal('rgb')) return false;
 
 			if ($this->literal('a')) $count = 4;
@@ -306,22 +504,78 @@ class lessc {
 				return false;
 			}
 
+			// grab the numbers and format
 			foreach (range(1, $count) as $i) {
-				
+				if (!$this->unit($color[], array('%')) || !($i == $count || $this->literal(','))) {
+					$this->seek($s);
+					return false;
+				}
+
+				if ($color[$i][0] == '%')
+					$color[$i] = 255 * ($color[$i][1] / 100);
+				else
+					$color[$i] = $color[$i][1];
 			}
 
-			if (!$this->literal('(')) {
+			if (!$this->literal(')')) {
 				$this->seek($s);
 				return false;
 			}
 		
-			$out = $color;
+			$out = $this->fixColor($color);
 			return true;
 		}
 
 		return false;
 	}
 
+	// consume a list of property values delimited by ; and wrapped in ()
+	function argumentValues(&$args, $delim = ';') {
+		$s = $this->seek();
+		if (!$this->literal('(')) return false;
+
+		$values = array();
+		while ($this->propertyValue($value)) {
+			$values[] = $value;
+			if (!$this->literal($delim)) break;
+		}
+
+		if (!$this->literal(')')) {
+			$this->seek($s);
+			return false;
+		}
+		
+		$args = $values;
+		return true;
+	}
+
+	// consume an argument definition list surrounded by (), each argument is a variable name with optional value
+	function argumentDef(&$args, $delim = ';') {
+		$s = $this->seek();
+		if (!$this->literal('(')) return false;
+
+		$values = array();
+		while ($this->variable($vname)) {
+			$arg = array($vname);
+			if ($this->literal(':') && $this->propertyValue($value)) {
+				$arg[] = $value;
+				// let the : slide if there is no value
+			}
+
+			$values[] = $arg;
+			if (!$this->literal($delim)) break;
+		}
+
+		if (!$this->literal(')')) {
+			$this->seek($s);
+			return false;
+		}
+
+		$args = $values;
+		return true;
+	}
+
+	// consume a list of tags
 	function tags(&$tags, $simple = false, $delim = ',') {
 		$tags = array();
 		while ($this->tag($tt, $simple)) {
@@ -333,9 +587,10 @@ class lessc {
 		return true;
 	}
 
+	// a single tag
 	function tag(&$tag, $simple = false) {
 		if ($simple)
-			$chars = '^,:;{}\][>\(\)';
+			$chars = '^,:;{}\][>\(\) ';
 		else
 			$chars = '^,;{}';
 
@@ -347,6 +602,19 @@ class lessc {
 		return true;
 	}
 
+	// a css function
+	function func(&$func) {
+		$s = $this->seek();
+
+		if ($this->keyword($fname) && $this->literal('(') && $this->to(')', $args)) {
+			$func = $fname.'('.$args.')';
+			return true;
+		}
+		$this->seek($s);
+		return false;
+	}
+
+	// consume a less variable
 	function variable(&$name) {
 		$s = $this->seek();
 		if ($this->literal('@', false) && $this->keyword($name)) {
@@ -355,6 +623,7 @@ class lessc {
 		return false;
 	}
 
+	// consume a keyword
 	function keyword(&$word) {
 		if ($this->match('([\w_\-!"][\w\-_"]*)', $m)) {
 			$word = $m[1];
@@ -363,8 +632,15 @@ class lessc {
 		return false;
 	}
 
+	// consume an end of statement delimiter
 	function end() {
-		return $this->literal(';');
+		if ($this->literal(';'))
+			return true;
+		elseif ($this->buffer{$this->count} == '}') {
+			// if there is a closing block next then we don't need a ;
+			return true;
+		}
+		return false;
 	}
 
 	function compressList($items, $delim) {
@@ -421,6 +697,24 @@ class lessc {
 			return $value[1];
 		case 'expression':
 			return $this->compileValue($this->evaluate($value[1], $value[2], $value[3]));
+		case 'string':
+			// search for inline variables to replace
+			$replace = array();
+			if (preg_match_all('/{(@[\w-_][0-9\w-_]*)}/', $value[1], $m)) {
+				foreach($m[1] as $name) {
+					if (!isset($replace[$name]))
+						$replace[$name] = $this->compileValue(array('variable', $name));
+				}
+			}
+			foreach ($replace as $var=>$val) {
+				// strip quotes
+				if (preg_match('/^(["\']).*?(\1)$/', $val)) {
+					$val = substr($val, 1, -1);
+				}
+				$value[1] = str_replace('{'.$var.'}', $val, $value[1]);
+			}
+
+			return $value[1];
 		case 'color':
 			if (count($value) == 5) { // rgba
 				return 'rgba('.$value[1].','.$value[2].','.$value[3].','.$value[4].')';
@@ -492,29 +786,30 @@ class lessc {
 	}
 
 	// make sure a color's components don't go out of bounds
-	private function fixColor($c) {
-		for ($i = 1; $i < 4; $i++) {
+	function fixColor($c) {
+		foreach (range(1, 3) as $i) {
 			if ($c[$i] < 0) $c[$i] = 0;
 			if ($c[$i] > 255) $c[$i] = 255;
 			$c[$i] = floor($c[$i]);
 		}
+
 		return $c;
 	}
 
-	private function op_number_color($op, $lft, $rgt) {
+	function op_number_color($op, $lft, $rgt) {
 		if ($op == '+' || $op = '*') {
 			return $this->op_color_number($op, $rgt, $lft);
 		}
 	}
 
-	private function op_color_number($op, $lft, $rgt) {
+	function op_color_number($op, $lft, $rgt) {
 		if ($rgt[0] == '%') $rgt[1] /= 100;
 
 		return $this->op_color_color($op, $lft,
 			array('color', $rgt[1], $rgt[1], $rgt[1]));
 	}
 
-	private function op_color_color($op, $lft, $rgt) {
+	function op_color_color($op, $lft, $rgt) {
 		$newc = array('color');
 
 		switch ($op) {
@@ -596,13 +891,13 @@ class lessc {
 	}
 
 	// push a new environment
-	private function push() {
+	function push() {
 		$this->level++;
 		$this->env[] = array();
 	}
 
 	// pop environment off the stack
-	private function pop() {
+	function pop() {
 		if ($this->level == 1)
 			throw new exception('parse error: unexpected end of block');
 
@@ -611,24 +906,24 @@ class lessc {
 	}
 
 	// set something in the current env
-	private function set($name, $value) {
+	function set($name, $value) {
 		$this->env[count($this->env) - 1][$name] = $value;
 	}
 
 	// append to array in the current env
-	private function append($name, $value) {
+	function append($name, $value) {
 		$this->env[count($this->env) - 1][$name][] = $value;
 	}
 
 	// put on the front of the value
-	private function prepend($name, $value) {
+	function prepend($name, $value) {
 		if (isset($this->env[count($this->env) - 1][$name]))
 			array_unshift($this->env[count($this->env) - 1][$name], $value);
 		else $this->append($name, $value);
 	}
 
 	// get the highest occurrence of value
-	private function get($name, $env = null) {
+	function get($name, $env = null) {
 		if (empty($env)) $env = $this->env;
 
 		for ($i = count($env) - 1; $i >= 0; $i--)
@@ -640,7 +935,7 @@ class lessc {
 	// get the most recent value of a variable
 	// return default if it isn't found
 	// $skip is number of vars to skip
-	private function getVal($name, $skip = 0, $default = array('keyword', '')) {
+	function getVal($name, $skip = 0, $default = array('keyword', '')) {
 		$val = $this->get($name);
 		if ($val == null) return $default;
 
@@ -665,7 +960,7 @@ class lessc {
 	}
 
 	// merge a block into the current env
-	private function merge($name, $value) {
+	function merge($name, $value) {
 		// if the current block isn't there then just set
 		$top =& $this->env[count($this->env) - 1];
 		if (!isset($top[$name])) return $this->set($name, $value);
@@ -694,8 +989,15 @@ class lessc {
 		return $this->match($this->preg_quote($what), $m, $eatWhitespace);
 	}
 
-	private function preg_quote($what) {
+	function preg_quote($what) {
 		return preg_quote($what, '/');
+	}
+
+	// match to $what, doesn't include in output
+	function to($what, &$out) {
+		if (!$this->match('(.*?)'.$this->preg_quote($what), $m)) return false;
+		$out = $m[1];
+		return true;
 	}
 	
 	// try to match something on head of buffer
@@ -703,10 +1005,8 @@ class lessc {
 		$r = '/'.$regex.($eatWhitespace ? '\s*' : '').'/Ais';
 		if (preg_match($r, $this->buffer, $out, null, $this->count)) {
 			$this->count += strlen($out[0]);
-			$this->tests[] = 'PASS: '.$r;
 			return true;
 		}
-		$this->tests[] = 'FAIL: '.$r;
 		return false;
 	}
 
@@ -715,11 +1015,9 @@ class lessc {
 	function peek($regex, &$out = null) {
 		$r = '/'.$regex.'/Ais';
 		$result =  preg_match($r, $this->buffer, $out, null, $this->count);
-		$this->tests[] = 'PEEK '.$result.': '.$r;
 		
 		return $result;
 	}
-
 
 	// seek to a spot in the buffer or return where we are on no argument
 	function seek($where = null) {
@@ -734,7 +1032,7 @@ class lessc {
 
 		$this->env = array();
 		$this->expandStack = array();
-		$this->depth = $this->count = 0;
+		$this->count = 0;
 		$this->line = 1;
 
 		$this->buffer = $this->removeComments($this->buffer);
@@ -753,19 +1051,18 @@ class lessc {
 		}
 
 		if ($this->count != strlen($this->buffer))  {
+			$line = $this->line + substr_count(substr($this->buffer, 0, $this->count), "\n");
 			if ($this->peek("(.*?)\n", $m))
-				throw new exception('parse error: failed at `'.$m[1].'`');
+				throw new exception('parse error: failed at `'.$m[1].'` line: '.$line);
 		}
 
 		if (count($this->env) > 1)
 			throw new exception('failed to parse: unclosed block');
 
 
-		print_r($this->env);
-		// print_r($this->tests);
+		// print_r($this->env);
 		return $out;
 	}
-
 
 	function __construct($fname = null) {
 		if (!self::$operatorString) {
@@ -787,6 +1084,7 @@ class lessc {
 
 	// remove comments from $text
 	// todo: make it work for all functions, not just url
+	// todo: make it not mess up line counter with block comments
 	function removeComments($text) {
 		$out = '';
 
@@ -822,8 +1120,20 @@ class lessc {
 			}
 		}
 
-		$this->count = 0;
 		return $out;
+	}
+
+
+	// compile to $in to $out if $in is newer than $out
+	// returns true when it compiles, false otherwise
+	public static function ccompile($in, $out) {
+		if (!is_file($out) || filemtime($in) > filemtime($out)) {
+			$less = new lessc($in);
+			file_put_contents($out, $less->parse());
+			return true;
+		}
+
+		return false;
 	}
 
 }
