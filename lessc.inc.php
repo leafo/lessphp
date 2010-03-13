b9bdd2433ea04389bad5d54ce61b47ad9885d95a
leaf corcoran <leafot@gmail.com>
2010-03-13 11:34:05 -0500

    Added ms unit

--- lessc.inc.php
+++ lessc.inc.php
@@ -40,7 +40,7 @@ class lessc {
 
 	static private $dtypes = array('expression', 'variable', 'function', 'negative'); // types with delayed computation
 	static private $units = array(
-		'px', '%', 'in', 'cm', 'mm', 'em', 'ex', 'pt', 'pc', 's');
+		'px', '%', 'in', 'cm', 'mm', 'em', 'ex', 'pt', 'pc', 'ms', 's');
 
 	public $importDisabled = false;
 	public $importDir = '';
