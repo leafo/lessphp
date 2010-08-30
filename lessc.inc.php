fa888f69246a3942adac65de997e270114bc1d49
Beau Simensen <beau@dflydev.com>
2010-08-30 16:55:19 -0700

    o First pass at supporting tracking internal import cache information.

--- lessc.inc.php
+++ lessc.inc.php
@@ -25,6 +25,8 @@ class lessc {
 
 	private $env = array();
 
+	private $allParsedFiles = array();
+
 	public $vPrefix = '@';
 	public $mPrefix = '$';
 	public $imPrefix = '!';
@@ -193,6 +195,7 @@ class lessc {
 
 			$full = $this->importDir.$url;
 			if (file_exists($file = $full) || file_exists($file = $full.'.less') || file_exists($this->normalizePath($file = $full)) || file_exists($this->normalizePath($file = $full.'.less'))) {
+				$this->addParsedFile($file);
 				$loaded = ltrim($this->removeComments(file_get_contents($file).";"));
 				$this->buffer = substr($this->buffer, 0, $this->count).$loaded.substr($this->buffer, $this->count);
 				return true;
@@ -1322,6 +1325,8 @@ class lessc {
 			$this->fileName = $fname;
 			$this->importDir = $pi['dirname'].'/';
 			$this->buffer = file_get_contents($fname);
+
+			$this->addParsedFile($fname);
 		}
 	}
 
@@ -1366,6 +1371,11 @@ class lessc {
 		return $out;
 	}
 
+	public function allParsedFiles() { return $this->allParsedFiles; }
+	protected function addParsedFile($file) {
+		$this->allParsedFiles[realpath($file)] = filemtime($file);
+	}
+
 
 	// compile to $in to $out if $in is newer than $out
 	// returns true when it compiles, false otherwise
@@ -1379,8 +1389,72 @@ class lessc {
 		return false;
 	}
 
+	/**
+	 * Execute lessphp on a .less file or a lessphp cache structure
+	 * 
+	 * The lessphp cache structure contains information about a specific
+	 * less file having been parsed. It can be used as a hint for future
+	 * calls to determine whether or not a rebuild is required.
+	 * 
+	 * The cache structure contains two important keys that may be used
+	 * externally:
+	 * 
+	 * compiled: The final compiled CSS
+	 * updated: The time (in seconds) the CSS was last compiled
+	 * 
+	 * The cache structure is a plain-ol' PHP associative array and can
+	 * be serialized and unserialized without a hitch.
+	 * 
+	 * @param mixed $in Input
+	 * @param bool $force Force rebuild?
+	 * @return array lessphp cache structure
+	 */
+	public static function cexecute($in, $force = false) {
+
+		// assume no root
+		$root = null;
+
+		if ( is_string($in) ) {
+			$root = $in;
+		} elseif ( is_array($in) and isset($in['root']) ) {
+			if ( $force or ! isset($in['files']) ) {
+				// If we are forcing a recompile or if for some reason the
+				// structure does not contain any file information we should
+				// specify the root to trigger a rebuild.
+				$root = $in['root'];
+			} elseif ( isset($in['files']) and is_array($in['files']) ) {
+				foreach ( $in['files'] as $fname => $ftime ) {
+					if ( ! file_exists($fname) or filemtime($fname) > $ftime ) {
+						// One of the files we knew about previously has changed
+						// so we should look at our incoming root again.
+						$root = $in['root'];
+						break;
+					}
+				}
+			}
+		} else {
+			// TODO: Throw an exception? We got neither a string nor something
+			// that looks like a compatible lessphp cache structure.
+			return null;
+		}
+
+		if ( $root !== null ) {
+			// If we have a root value which means we should rebuild.
+			$less = new lessc($root);
+			$out = array();
+			$out['root'] = $root;
+			$out['compiled'] = $less->parse();
+			$out['files'] = $less->allParsedFiles();
+			$out['updated'] = time();
+			return $out;
+		} else {
+			// No changes, pass back the structure
+			// we were given initially.
+			return $in;
+		}
+
+	}
+
 }
-
-
 
 ?>
