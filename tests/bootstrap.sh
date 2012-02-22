541f64d1acfefd9aab064a4ebeec21ad7eb13ba9
leaf corcoran <leafot@gmail.com>
2012-02-21 20:31:44 -0800

    fixed typos and enhanced script #199

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,29 +1,45 @@
 echo "This script clones twitter bootsrap, compiles it with lessc and lessphp,"
 echo "cleans up results with csstidy, and outputs diff. To run it, you need to"
 echo "have git, csstidy and lessc installed."
+echo ""
 
-csstidy_params="--allow_html_in_templates=false --compress_colors=false --compress_font-weight=false --discard_invalid_properties=false --lowercase_s=false --preserve_css=true --remove_bslash=false --remove_last\;=false --sort-properties=true --sort-selectors=true --timestamp=false --silent=true --merge_selectors=0 --case-properties=0 --optimize-shorthands=0 --template=high"
+csstidy_params="--allow_html_in_templates=false --compress_colors=false
+--compress_font-weight=false --discard_invalid_properties=false
+--lowercase_s=false --preserve_css=true --remove_bslash=false
+--remove_last_;=false --sort-properties=true --sort-selectors=true
+--timestamp=false --silent=true --merge_selectors=0 --case-properties=0
+--optimize-shorthands=0 --template=high"
 
-diff_params="-b -u -t -B"
-
-if [ ! -d 'tmp/' ]
-then
-  mkdir tmp/
+if [ -z "$@" ]; then
+  diff_tool="diff -b -u -t -B"
+else
+  diff_tool=$@
 fi
 
-if [ ! -d 'bootsrap/' ]
-then
+mkdir -p tmp
+
+if [ ! -d 'bootstrap/' ]; then
   echo ">> Cloning bootstrap to bootstrap/"
   git clone https://github.com/twitter/bootstrap
 fi
 
 echo ">> Lessc compilation"
 lessc bootstrap/less/bootstrap.less tmp/bootstrap.lessc.css
+
 echo ">> Lessphp compilation"
-../plessc bootstrap/less/bootstrap.less tmp/bootstrap.lessphp.csstidy
+../plessc bootstrap/less/bootstrap.less tmp/bootstrap.lessphp.css
 echo ">> Cleanup and convert"
 csstidy tmp/bootstrap.lessc.css $csstidy_params tmp/bootstrap.lessc.clean.css
 csstidy tmp/bootstrap.lessphp.css $csstidy_params tmp/bootstrap.lessphp.clean.css
+
+# put a newline after { and :
+function split() {
+  sed 's/\(;\|{\)/\1\n/g'
+}
+
+# csstidy is messed up and wont output to stdout when there are a bunch of options
+cat tmp/bootstrap.lessc.clean.css | split | tee tmp/bootstrap.lessc.clean.css
+cat tmp/bootstrap.lessphp.clean.css | split | tee tmp/bootstrap.lessphp.clean.css
+
 echo ">> Doing diff"
-diff $diff_params tmp/bootstrap.lessc.clean.css tmp/bootstrap.lessphp.clean.css > tmp/diff
-cat tmp/diff
+$diff_tool tmp/bootstrap.lessc.clean.css tmp/bootstrap.lessphp.clean.css
