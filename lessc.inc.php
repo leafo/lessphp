96600236c6c7f776012572d852e3e91a74712248
Tor Edvardsson <tor.edvardsson@gmail.com>
2012-05-10 17:58:02 +0300

    Throw error if import file does not exist.

--- lessc.inc.php
+++ lessc.inc.php
@@ -329,7 +329,9 @@ class lessc {
 			$full = $dir.(substr($dir, -1) != '/' ? '/' : '').$url;
 			if ($this->fileExists($file = $full.'.less') || $this->fileExists($file = $full)) {
 				return $file;
-			}
+			} else {
+		     		$this->throwError("File '$file' does not exist");
+       			}
 		}
 
 		return null;
