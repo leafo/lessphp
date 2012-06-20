988f6ec4ac865acbb5d9a3bcfac54d28c9f1976e
leaf corcoran <leafot@gmail.com>
2012-06-19 21:07:39 -0700

    Revert "remove compress_colors"
    This reverts commit fbfcf7a344d86d7d0ae1db5b45710b0e3934de9d.

--- lessc.inc.php
+++ lessc.inc.php
@@ -583,9 +583,11 @@ class lessc {
 
 			$h = sprintf("#%02x%02x%02x", $r, $g, $b);
 
-			// Converting hex color to short notation (e.g. #003399 to #039) 
-			if ($h[1] === $h[2] && $h[3] === $h[4] && $h[5] === $h[6]) {
-				$h = '#' . $h[1] . $h[3] . $h[5];
+			if (!empty($this->formatter->compress_colors)) {
+				// Converting hex color to short notation (e.g. #003399 to #039) 
+				if ($h[1] === $h[2] && $h[3] === $h[4] && $h[5] === $h[6]) {
+					$h = '#' . $h[1] . $h[3] . $h[5];
+				}
 			}
 
 			return $h;
@@ -2825,6 +2827,8 @@ class lessc_formatter {
 	public $openSingle = " { ";
 	public $closeSingle = " }";
 
+	public $compress_colors = false;
+
 	// returns the amount of indent that should happen for a block
 	function indentAmount($block) {
 		return isset($block->isRoot) || !empty($block->no_multiply) ? 1 : 0;
@@ -2896,6 +2900,8 @@ class lessc_formatter_compressed extends lessc_formatter {
 	public $close = "}";
 	public $tagSeparator = ",";
 	public $disableSingle = true;
+
+	public $compress_colors = true;
 }
 
 class lessc_formatter_indent extends lessc_formatter {
