a62839470b01d90903484cc18dc0f476059354ec
Benoit Pruneau <bpruneau@gmail.com>
2012-08-09 12:56:35 -0300

    unsetVariable(): replace "name" with "$name"

--- lessc.inc.php
+++ lessc.inc.php
@@ -1738,7 +1738,7 @@ class lessc {
 	}
 
 	public function unsetVariable($name) {
-		unset($this->registeredVars[name]);
+		unset($this->registeredVars[$name]);
 	}
 
 	public function allParsedFiles() {
