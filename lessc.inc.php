a89fc09711aa9bb362f682cfb15995698384f867
Dmitri Pyatkov <dkrnl@yandex.ru>
2014-05-29 13:20:16 +0800

    detect mime via file extension

--- lessc.inc.php
+++ lessc.inc.php
@@ -1005,9 +1005,23 @@ class lessc {
 						$mime = $mime[0];
 					} elseif(function_exists('mime_content_type')) { // PHP 5.2
 						$mime = mime_content_type($fullpath);
-					} elseif(function_exists('getimagesize') && ($imagesize = @getimagesize($fullpath))) {
-						$mime = image_type_to_mime_type($imagesize[2]);
-					} 
+					} else {
+						$types = array(
+							'gif' => 'image/gif',
+							'jpg' => 'image/jpeg',
+							'jpeg'=> 'image/jpeg',
+							'png' => 'image/png',
+							'ttf' => 'application/x-font-ttf',
+							'otf' => 'application/x-font-otf',
+							'eot' => 'application/vnd.ms-fontobject',
+							'woff' => 'application/x-font-woff',
+							'svg' => 'image/svg+xml',
+						);
+						$ext = strtolower(pathinfo($fullpath, PATHINFO_EXTENSION));
+						if (isset($types[$ext])) {
+							$mime = $types[$ext];
+						}
+					}
 				}
 
 				if(!is_null($mime)) { // fallback if the mime type is still unknown
