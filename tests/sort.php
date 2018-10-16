cffe518d69b56c630c0d64fcce44c83931c0354a
Timo Tijhof <krinklemail@gmail.com>
2018-10-16 06:02:07 +0100

    tests: Apply PSR2 style for 'Generic.WhiteSpace.DisallowTabIndent'
    I'm already starting to regret this. Obviously, tabs are the only
    correct way of indenting code. But alas, PSR2 disagrees and being
    different makes the code harder to deal with for those familiar
    with PSR2.
    
    I'd be fine with switching to a different popular style preset,
    but we shouldn't have our own mix of PSR2 and something else,
    so for let's just go all-in on PSR2.

--- tests/sort.php
+++ tests/sort.php
@@ -8,46 +8,46 @@ require realpath(dirname(__FILE__)).'/../lessc.inc.php';
 $exe = array_shift($argv); // remove filename
 
 if (!$fname = array_shift($argv)) {
-	$fname = "php://stdin";
+    $fname = "php://stdin";
 }
 
 class lesscNormalized extends lessc {
-	public $numberPrecision = 3;
+    public $numberPrecision = 3;
 
-	public function compileValue($value) {
-		if ($value[0] == "raw_color") {
-			$value = $this->coerceColor($value);
-		}
+    public function compileValue($value) {
+        if ($value[0] == "raw_color") {
+            $value = $this->coerceColor($value);
+        }
 
-		return parent::compileValue($value);
-	}
+        return parent::compileValue($value);
+    }
 }
 
 class SortingFormatter extends lessc_formatter_lessjs {
-	function sortKey($block) {
-		if (!isset($block->sortKey)) {
-			sort($block->selectors, SORT_STRING);
-			$block->sortKey = implode(",", $block->selectors);
-		}
+    function sortKey($block) {
+        if (!isset($block->sortKey)) {
+            sort($block->selectors, SORT_STRING);
+            $block->sortKey = implode(",", $block->selectors);
+        }
 
-		return $block->sortKey;
-	}
+        return $block->sortKey;
+    }
 
-	function sortBlock($block) {
-		usort($block->children, function($a, $b) {
-			$sort = strcmp($this->sortKey($a), $this->sortKey($b));
-			if ($sort == 0) {
-				// TODO
-			}
-			return $sort;
-		});
+    function sortBlock($block) {
+        usort($block->children, function($a, $b) {
+            $sort = strcmp($this->sortKey($a), $this->sortKey($b));
+            if ($sort == 0) {
+                // TODO
+            }
+            return $sort;
+        });
 
-	}
+    }
 
-	function block($block) {
-		$this->sortBlock($block);
-		return parent::block($block);
-	}
+    function block($block) {
+        $this->sortBlock($block);
+        return parent::block($block);
+    }
 
 }
 
